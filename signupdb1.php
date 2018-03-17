2c7d6e4fb6f7fdcfc821277cfb18a9915652c5f3
Hanlak <22263473+Hanlak@users.noreply.github.com>
2018-03-17 23:54:06 +0530

    Update signupdb1.php

--- signupdb1.php
+++ signupdb1.php
@@ -72,7 +72,8 @@ else
 {
      if($password==$confirm_password)
      {
-           
+           $sql_crt = "CREATE TABLE IF NOT EXISTS register ( `id` INT(100) NOT NULL AUTO_INCREMENT , `username` VARCHAR(100) NOT NULL , `email` VARCHAR(100) NOT NULL , `password` VARCHAR(100) NOT NULL , `phone` VARCHAR(15) NOT NULL , `checkotp` TINYINT(1) NOT NULL DEFAULT '0' , PRIMARY KEY (`id`, `email`), UNIQUE (`phone`)) ENGINE = InnoDB;";
+		mysqli_query($conn,$sql_crt);
          $sql= "INSERT into register(username, email, password, phone) values('$name','$email','$password','$phone')";
 		 if(mysqli_query($conn,$sql))
          
